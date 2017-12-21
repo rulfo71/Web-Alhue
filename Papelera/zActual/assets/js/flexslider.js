f796bd721f44f7d6892d8bdcbc1adf12180a9890
tomaspulenta <tpulenta@gmail.com>
2017-12-21 10:35:17 +0100

    Initial commit

---
+++ Papelera/zActual/assets/js/flexslider.js
@@ -0,0 +1,37 @@
+$(function(){
+  var toggles = $('.toggle a'),
+      codes = $('.code');
+  
+  toggles.on("click", function(event){
+    event.preventDefault();
+    var $this = $(this);
+    
+    if (!$this.hasClass("active")) {
+      toggles.removeClass("active");
+      $this.addClass("active");
+      codes.hide().filter(this.hash).show();
+    }
+  });
+  toggles.first().click();
+});
+
+$(window).load(function() {
+	// The slider being synced must be initialized first
+	$('#carousel').flexslider({
+		animation: "slide",
+		controlNav: false,
+		animationLoop: false,
+		slideshow: false,
+		itemWidth: 210,
+		itemMargin: 5,
+		asNavFor: '#slider'
+	});
+
+	$('#slider').flexslider({
+		animation: "slide",
+		controlNav: false,
+		animationLoop: false,
+		slideshow: false,
+		sync: "#carousel"
+	});
+});
